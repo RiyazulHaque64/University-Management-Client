6306ee74d9b51db6c7d98f644044dfba3bd97a82
RiyazulHaque64 <riyazulhaque64@gmail.com>
2024-01-20 02:55:40 +0600

    Update admin routes

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,11 +1,29 @@
-import { Layout, Menu } from "antd";
-import { Outlet } from "react-router-dom";
+import { Layout, Menu, MenuProps } from "antd";
+import { NavLink, Outlet } from "react-router-dom";
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [
+const items: MenuProps["items"] = [
   {
-    key: "admin",
-    level: "User Mangement",
+    key: "Dashboard",
+    label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
+  },
+  {
+    key: "User Management",
+    label: "User Management",
+    children: [
+      {
+        key: "Create Admin",
+        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
+      },
+      {
+        key: "Create Faculty",
+        label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
+      },
+      {
+        key: "Create Student",
+        label: <NavLink to="/admin/create-student">Create Student</NavLink>,
+      },
+    ],
   },
 ];
 
